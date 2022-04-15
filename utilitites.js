24740ef9b1b37248d3c629c277aa46ac8dc55f34
Aibar Manas <aibarmanas@gmail.com>
2022-04-15 18:11:57 +0600

    utilities start

---
+++ utilitites.js
@@ -0,0 +1,26 @@
+// Check values if letter or number
+String.prototype.isLetter = function() {
+    return this.length === 1 && this.match(/[A-Z|a-z]/i);
+}
+
+String.prototype.isNum = function (){
+    if(isNaN(parseInt(this)))return false;
+    if(!isNaN(parseInt(this)))return true;
+}
+
+// Selection utilitites function
+const selection = window.getSelection()
+const headerElement = document.querySelector('#question-header a')
+selection.setBaseAndExtent(headerElement,0,headerElement,1)
+
+
+function highlight(text) {
+    var inputText = document.getElementById("inputText");
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) { 
+        innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+        inputText.innerHTML = innerHTML;
+    }
+}
+
